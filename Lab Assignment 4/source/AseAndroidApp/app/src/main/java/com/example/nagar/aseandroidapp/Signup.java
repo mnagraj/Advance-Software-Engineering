616621594aa1071d3269ad3e779f7dfa417c35e9
nagarajureddymyaka <nmcyb@mail.umkc.edu>
2016-09-23 19:03:00 -0500

    changes made

---
+++ Lab Assignment 4/source/AseAndroidApp/app/src/main/java/com/example/nagar/aseandroidapp/Signup.java
@@ -0,0 +1,50 @@
+package com.example.nagar.aseandroidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Signup extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+    public void onSignupClick(View v)
+    {
+        if(v.getId() == R.id.Bsignupbutton)
+        {
+            EditText name = (EditText) findViewById(R.id.TFname);
+            EditText email = (EditText) findViewById(R.id.TFemail);
+            EditText username = (EditText) findViewById(R.id.TFusername);
+            EditText pass1 = (EditText) findViewById(R.id.TFpass1);
+            EditText pass2= (EditText) findViewById(R.id.TFpass2);
+
+
+            String namestr =name.getText().toString();
+            String emailstr =email.getText().toString();
+            String usernamestr =username.getText().toString();
+            String pass1str =pass1.getText().toString();
+            String pass2str =pass2.getText().toString();
+            if(!pass1str.equals(pass2str))
+            {
+                //popup msg
+                Toast pass = Toast.makeText(Signup.this,"Passwords did't match!",Toast.LENGTH_SHORT);
+                pass.show();
+
+
+            }
+
+
+
+        }
+
+
+    }
+
+
+}
